f23889db3d8d575f673b6479b0540e77bcd19597
Anthony Gore <anthony@magnum.technology>
2017-02-17 13:54:36 +0700

    07

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import './style.scss';
 
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
+
 import App from './components/App.vue';
 
 new Vue({
   el: '#app',
+  data: {
+    moment
+  },
   components: {
     App
   }
