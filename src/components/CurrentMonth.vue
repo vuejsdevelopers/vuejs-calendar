ede67f96b2fc9e3f9ef54b48d1d84d7e21ee9d6e
Anthony Gore <anthony@magnum.technology>
2017-02-17 17:40:01 +0700

    20

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,8 +1,18 @@
 <template>
-
+    <div>{{ formattedDate }}</div>
 </template>
 <script>
     export default {
-
+        computed: {
+            formattedDate() {
+                return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
+            },
+            month() {
+                return this.$store.state.currentMonth;
+            },
+            year() {
+                return this.$store.state.currentYear;
+            },
+        }
     }
 </script>
