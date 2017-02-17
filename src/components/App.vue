37f7caf5b680bb94a76a50d88de2ff97f5a7d12f
Anthony Gore <anthony@magnum.technology>
2017-02-17 16:55:31 +0700

    16

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        <div v-for="week in weeks">
-            Week
-            <div v-for="day in week">{{ day }}</div>
+        <div id="day-bar">
+            <div>Mon</div>
+            <div>Tue</div>
+            <div>Wed</div>
+            <div>Thu</div>
+            <div>Fri</div>
+            <div>Sat</div>
+            <div>Sun</div>
+        </div>
+        <div id="calendar">
+            <div v-for="week in weeks" class="calendar-week">
+                <calendar-day v-for="day in week" :day="day"></calendar-day>
+            </div>
         </div>
     </div>
 </template>
 <script>
+    import CalendarDay from './CalendarDay.vue';
+
     export default {
         data() {
             return {
@@ -64,6 +76,9 @@
 
                 return weeks;
             }
+        },
+        components: {
+            CalendarDay
         }
     }
 </script>
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="classObject">{{ day.format('D') }}</div>
+</template>
+<script>
+    export default {
+        props: [ 'day' ],
+        computed: {
+            classObject() {
+                let today = this.day.isSame(this.$moment(), 'day');
+                return {
+                    day: true,
+                    today,
+                    past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
+                };
+            }
+        }
+    }
+</script>
