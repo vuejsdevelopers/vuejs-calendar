7670ac3ffed2048720c004a9def6cfaf89119e96
Anthony Gore <anthony@magnum.technology>
2017-02-18 11:06:13 +0700

    33

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,11 +1,19 @@
 <template>
     <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
         <h4>Add an event</h4>
+        <div class="text">
+            <input type="text" v-model="description">
+        </div>
         <button id="close-button" @click="close">&#10005</button>
     </div>
 </template>
 <script>
     export default {
+        data() {
+            return {
+                description: ''
+            }
+        },
         methods: {
            close() {
                this.$store.commit('eventFormActive', false);
